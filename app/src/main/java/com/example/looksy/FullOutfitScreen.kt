4ba961dbf7765e640e4fb073693cbb2f3cbe1b6b
bleikerja <bleikerja@gmail.com>
2025-10-03 13:10:26 +0200

    create prototype of FullOutfitScreen and ClothInformationScreen

--- app/src/main/java/com/example/looksy/FullOutfitScreen.kt
+++ app/src/main/java/com/example/looksy/FullOutfitScreen.kt
@@ -1,18 +1,56 @@
 package com.example.looksy
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.looksy.ui.theme.LooksyTheme
 
 @Composable
-fun fullOutfitScreen(){
+fun FullOutfitScreen(){
+    Column {
+        Text("Add Outfit", modifier = Modifier.align(Alignment.CenterHorizontally))
+        OutfitPart(painterResource(id = R.drawable.shirt))
+        OutfitPart(painterResource(id = R.drawable.jeans))
+    }
+}
+
+@Composable
+fun OutfitPart(image: Painter){
+    Row {
+        Image(
+            modifier = Modifier
+                .height(300.dp),
+            painter = image,
+            contentDescription = ""
+        )
+        LooksyButton(
+            { changeOutfit() },
+            Modifier.align(Alignment.CenterVertically),
+            { Icon(Icons.Default.Create, contentDescription = "") }
+        )
+    }
+}
+
+fun changeOutfit(){
 
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun FullOutfitPreview() {
     LooksyTheme {
-        fullOutfitScreen()
+        FullOutfitScreen()
     }
 }
--- app/src/main/java/com/example/looksy/MainActivity.kt
+++ app/src/main/java/com/example/looksy/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.looksy.ui.theme.LooksyTheme
 
@@ -19,29 +14,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LooksyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                FullOutfitScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name! This is the beginning of Looksy.",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     LooksyTheme {
-        Greeting("Android")
+        FullOutfitScreen()
     }
 }
