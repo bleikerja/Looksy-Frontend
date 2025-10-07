89d256dc9f9d7db6fa8fe38d9632931f24f6580d
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-10-07 12:44:07 +0200

    beginning of declaration of iconbutton

--- app/src/main/java/com/example/looksy/LooksyButton.kt
+++ app/src/main/java/com/example/looksy/LooksyButton.kt
@@ -1,29 +1,34 @@
 package com.example.looksy
 
-import androidx.compose.material3.Button
+import androidx.compose.foundation.Image
 import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun LooksyButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    picture: @Composable () ->Unit,
+    picture: Unit,
 ) {
     FilledIconButton(
         onClick = onClick,
         modifier = modifier,
-        enabled = TODO(),
-        colors = TODO(),
-        interactionSource = TODO()
     ) { picture }
 }
 
 @Preview
 @Composable
 fun PreviewLooksyButton() {
-    LooksyButton()
+    LooksyButton(
+        onClick = {},
+        modifier = Modifier,
+        picture =
+            Image(
+                painter = painterResource(id = R.drawable.unbenanntes),
+                contentDescription = ""
+            )
+    )
 }
