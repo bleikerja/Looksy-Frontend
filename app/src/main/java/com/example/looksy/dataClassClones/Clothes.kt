4a8b5605517cfdae3d28a052fd93b3b870ed9352
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-10-08 11:33:44 +0200

    clones for dataclasses

---
+++ app/src/main/java/com/example/looksy/dataClassClones/Clothes.kt
@@ -0,0 +1,8 @@
+package com.example.looksy.dataClassClones
+
+data class Clothes( val size:Size,
+val seasonUsage:Season,
+val type:Type,
+val material:Material,
+val clean:Boolean,
+val washingNotes:WashingNotes)
